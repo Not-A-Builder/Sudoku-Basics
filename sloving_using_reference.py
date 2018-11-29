b1c1a6a16ed4d04d7d240ff0e6fe21e2e88e2d44
Sav <dsouvik141@gmail.com>
2018-11-29 16:01:50 +0530

    Create sloving_using_reference.py
    In here, I tried to create 3 reference lists for rows, columns and blocks (3X3 matrix) and using them as a reference the program will put a random value in (j, i)th position that is present in jth row, ith column and (j//3+(i//3)*3)th block and after putting the value in sudokuGrid the program will erase that valuse from jth row, ith column and (j//3+(i//3)*3)th block. But, there is a flaw.
    As the program is chosing the values randomly there will be a higher possibility not to find out a number that is present in jth row, ith column and (j//3+(i//3)*3)th block.

---
+++ sloving_using_reference.py
@@ -0,0 +1,38 @@
+import random
+
+sudokuGrid = [] # the Sudoku
+dim = 3 # dimension of a single block
+grid_Dim = dim**2 # Sudoku dimension
+
+# INITIALIZING
+ref_Row, ref_Column, ref_Block = [], [], []
+for i in range(grid_Dim):
+    sudokuGrid.append(['N']*grid_Dim)
+#    creating the reference lists
+    ref_Row.append(list(range(1, grid_Dim + 1)))
+    ref_Column.append(list(range(1, grid_Dim + 1)))
+    ref_Block.append(list(range(1, grid_Dim + 1)))
+    
+def merge(R, C, B):
+#    returns a list of possible numbers we can put
+    poss = []
+    for i in range(grid_Dim):
+        if R[i] and B[i] and C[i]:
+            poss.append(R[i])
+    return poss
+
+for j in range(grid_Dim):
+    for i in range(grid_Dim):
+        k = i//dim + (j//dim)*dim
+        poss_List = merge(ref_Row[j], ref_Column[i], ref_Block[k])
+        if len(poss_List): # if there is a possibility then place the number
+            ind = random.randrange(len(poss_List)) # choosing a random number from the possible list
+            sudokuGrid[j][i] = poss_List[ind]
+            key = poss_List[ind] - 1
+            ref_Row[j][key] = 0
+            ref_Column[i][key] = 0
+            ref_Block[k][key] = 0
+	    
+for i in sudokuGrid:
+#   prints out the sudokuGrid, but it will not be a Sudoku
+    print(*i)
